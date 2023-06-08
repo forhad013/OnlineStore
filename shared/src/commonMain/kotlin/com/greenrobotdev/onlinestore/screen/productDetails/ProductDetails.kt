bc8a0ea862c180722d8d1e6bc08b014e46706803
forhad013 <forhad013@gmail.com>
2023-06-08 20:46:31 +0800

    Added typography

--- shared/src/commonMain/kotlin/com/greenrobotdev/onlinestore/screen/productDetails/ProductDetails.kt
+++ shared/src/commonMain/kotlin/com/greenrobotdev/onlinestore/screen/productDetails/ProductDetails.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
@@ -35,7 +34,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
